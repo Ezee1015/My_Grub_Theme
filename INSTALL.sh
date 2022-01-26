a898df8fa1e96f37ffffd844e1d8ffb6e74939c8
Ezee1015 <routerciscoanuitti@gmail.com>
2022-01-25 21:29:30 -0300

    Arregla Problemas

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,20 +12,22 @@ function INSTALAR {
    read -p "Ingrese la Seleccion > " respuesta
 
    case $respuesta in
-       '1' ) $DISTRO="debian" ;;
-       '2' ) $DISTRO="manjaro" ;;
-       '3' ) $DISTRO="mint" ;;
-       '4' ) $DISTRO="arch" ;;
-       '5' ) $DISTRO="kali" ;;
+       '1' ) DISTRO="debian" ;;
+       '2' ) DISTRO="manjaro" ;;
+       '3' ) DISTRO="mint" ;;
+       '4' ) DISTRO="arch" ;;
+       '5' ) DISTRO="kali" ;;
         *  ) INSTALAR ;;
     esac
 
     clear
     sudo cp -r "dark-matter-$DISTRO" /boot/grub/themes/
-    sudo echo "GRUB_THEME="/boot/grub/themes/dark-matter/theme.txt"" >> /etc/default/grub
+    # sudo echo "GRUB_THEME="/boot/grub/themes/dark-matter/theme.txt"" >> /etc/default/grub
 
     echo ""
     echo "Seleccine el Tema en el Grub Customizer"
+    echo ""
+    echo ""
     sudo grub-customizer
 
 }
