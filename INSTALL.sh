b8c5dbbc3462e5287a3b3286b7bfbe45546f3bdd
Ezee1015 <routerciscoanuitti@gmail.com>
2022-01-25 21:11:58 -0300

    Agrega los Archivos Bases

---
+++ INSTALL.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function INSTALAR {
+
+   clear
+   echo "Seleccione la Distro que Aparezca de Fondo en el GRUB..."
+   echo "   1.- DEBIAN"
+   echo "   2.- MANJARO"
+   echo "   3.- LINUX MINT"
+   echo "   4.- ARCH LINUX"
+   echo "   5.- KALI LINUX"
+   read -p "Ingrese la Seleccion > " respuesta
+
+   case $respuesta in
+       '1' ) $DISTRO="debian" ;;
+       '2' ) $DISTRO="manjaro" ;;
+       '3' ) $DISTRO="mint" ;;
+       '4' ) $DISTRO="arch" ;;
+       '5' ) $DISTRO="kali" ;;
+        *  ) INSTALAR ;;
+    esac
+
+    clear
+    sudo cp -r "dark-matter-$DISTRO" /boot/grub/themes/
+    sudo echo "GRUB_THEME="/boot/grub/themes/dark-matter/theme.txt"" >> /etc/default/grub
+
+    echo ""
+    echo "Seleccine el Tema en el Grub Customizer"
+    sudo grub-customizer
+
+}
+
+
+
+function MENU {
+
+    clear
+
+    for number in {0..10}; do
+        echo ""
+    done
+
+    echo "" ; echo ""
+    read -p "Desea Instalar Mi Tema de Grub? (S/N)>  " respuesta
+    clear
+    echo "" ; echo ""
+
+    case $respuesta in
+        's' )   INSTALAR  ;;
+        'si' )  INSTALAR  ;;
+        'Si' )  INSTALAR  ;;
+        'sI' )  INSTALAR  ;;
+        'SI' )  INSTALAR  ;;
+        'y' )   INSTALAR  ;;
+        'yes' ) INSTALAR  ;;
+        'YES' ) INSTALAR  ;;
+        '1' )   INSTALAR  ;;
+        'S' )   INSTALAR  ;;
+        * )     echo "           Se ha elegido que NO. Se saldrá del Instalador." ;;
+    esac
+
+    echo "" ; echo ""
+
+}
+
+MENU
+exit
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# My_Grub_Theme
-Mi Tema de GRUB2. Temas creados por vandalsoul, publicado en https://github.com/vandalsoul/darkmatter-grub2-theme
+# My_VIM_Customization
+Mi básica customización de VIM, creado por www.vim-bootstrap.com y personalizado por mi para mis necesidades.
+
+# REQUISITOS
+```
+grub2
+grub-customizer
+```
+
+## INSTALACIÓN
+Para Instalarlo, pegar estos comandos en la terminal uno por uno...
+```
+cd
+git clone https://github.com/Ezee1015/My_Grub_Theme
+cd My_VIM_Customization
+bash INSTALL.sh
+cd ..
+rm -rf My_Grub_Theme
+```
